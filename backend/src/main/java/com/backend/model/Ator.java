4b5f53f35ced4417643a0dcf4759f5fc2d811ba2
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-08-06 20:38:11 -0300

    Remoção Lógica (Soft Delete)

--- backend/src/main/java/com/backend/model/Ator.java
+++ backend/src/main/java/com/backend/model/Ator.java
@@ -1,8 +1,9 @@
 package com.backend.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
-import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "ator")
+@SQLDelete(sql = "UPDATE ator SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Ator {
 
     @Id
@@ -22,8 +27,14 @@ public class Ator {
     private Long id;
 
     @NotBlank
-    @NonNull
+    @NotNull
     @Length(min = 3, max = 200)
     @Column(name = "nome", length = 200, nullable = false)
     private String nome;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(name = "status", length = 10, nullable = false)
+    private String status = "Ativo";
 }
