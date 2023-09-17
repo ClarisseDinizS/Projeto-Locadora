7f942703bfcfc6f95598fac12d852bc35be3af6c
ClarisseDinizS <dinizclarisse.cd@gmail.com>
2023-09-17 11:47:41 -0300

    Listar Ator

--- Frontend/src/app/atores/atores/atores.component.ts
+++ Frontend/src/app/atores/atores/atores.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/error-dialog/error-dialog.component';
+
+import { Ator } from '../model/ator';
+import { AtoresService } from '../services/atores.service';
 
 @Component({
   selector: 'app-atores',
   templateUrl: './atores.component.html',
   styleUrls: ['./atores.component.scss']
 })
-export class AtoresComponent {
+export class AtoresComponent implements OnInit {
 
+  atores: Observable<Ator[]>;
+  colunasExibidas = ['nome'];
+
+  constructor(private atorServico: AtoresService, public dialog: MatDialog) {
+    this.atores = this.atorServico.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar atores')
+          return of([])
+        })
+      );
+    }
+
+    onError(message: string){
+      this.dialog.open(ErrorDialogComponent,{
+        data: message
+      })
+    }
+
+    ngOnInit(): void {
+
+    }
 }
---
+++ Frontend/src/app/atores/services/atores.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs/operators';
+
+import { Ator } from '../model/ator';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AtoresService {
+
+  private readonly API = 'api/atores';
+
+  constructor(private httpCliente: HttpClient) { }
+
+  list() {
+    return this.httpCliente.get<Ator[]>(this.API)
+      .pipe(
+        first(),
+        delay(5000),
+        tap(atores => console.log(atores))
+      );
+  }
+}
