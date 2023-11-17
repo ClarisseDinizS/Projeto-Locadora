6fbe25aa4eff8bc96f1912dd85ef3c4d4a1ac60e
Edson Lima <49738290+EdsonLima99@users.noreply.github.com>
2023-11-16 21:20:53 -0300

    Criado enum de Status

---
+++ Backend/src/main/java/com/backend/enums/converters/StatusConverter.java
@@ -0,0 +1,29 @@
+package com.backend.enums.converters;
+
+import java.util.stream.Stream;
+
+import com.backend.enums.Tipo;
+
+import jakarta.persistence.AttributeConverter;
+
+public class StatusConverter implements AttributeConverter<Tipo, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Tipo tipo) {
+        if (tipo == null) {
+            return null;
+        }
+        return tipo.getValor();
+    }
+
+    @Override
+    public Tipo convertToEntityAttribute(String valor) {
+        if (valor == null) {
+            return null;
+        }
+        return Stream.of(Tipo.values())
+                .filter(t -> t.getValor().equals(valor))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
